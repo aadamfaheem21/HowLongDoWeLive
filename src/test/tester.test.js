298f0493848a7df9615d6457102d3dae3223337a
Cassie Elish <cassieelish@gmail.com>
2020-12-14 12:55:51 -0500

    more progress with tester.test.js

--- src/test/tester.test.js
+++ src/test/tester.test.js
@@ -1,54 +1,51 @@
 const fs = require("fs");
 let stateNames = fs.readFileSync('../statenames.json');
-// let countyNames = fs.readFileSync('../countyLookup.json');
-
 
 for (state of stateNames) {
-  fs.
+  let state = fs.readFileSync('../jsons/'+state+'.json');
+
+  describe('Checking data format', ()=> {
+    test("All %'s should be represented as decimals'", () =>) {
+      expect(true).toEqual(true);
+    });
+  });
+
+  describe('Checking data completeness', ()=> {
+    test("All states should be represented", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All states should have data from 1987-2009", () =>) {
+      expect(states).not.toEqual("");
+    });
+
+    test("All counties should be represented", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All total male life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All total female life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All white female life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All white male life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All black male life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+    test("All black female life expectancies should be filled in", () =>) {
+      expect(true).toEqual(true);
+    });
+
+  });
 }
-
-
-describe('Checking data format', ()=> {
-  test("All %'s should be represented as decimals'", () =>) {
-    expect(true).toEqual(true);
-  });
-});
-
-describe('Checking data completeness', ()=> {
-  test("All states should be represented", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All states should have data from 1987-2009", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All counties should be represented", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All total male life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All total female life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All white female life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All white male life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All black male life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-  test("All black female life expectancies should be filled in", () =>) {
-    expect(true).toEqual(true);
-  });
-
-});
